2ef8e452eea9513de5e3947b27289fec357a38de
Ivan Kabadzhov <kabadzhovivan@gmail.com>
2023-02-08 22:20:45 +0100

    Minimal example following the presentation

--- README.md
+++ README.md
@@ -1,2 +1,214 @@
 # iCSC-Profiling
 A small profiling example presented in the iCSC
+
+
+## Before starting
+
+### Need to allow monitoring events
+
+```
+sudo sysctl kernel.perf_event_paranoid=-1
+sudo sysctl kernel.kptr_restrict=0
+sudo sysctl kernel.nmi_watchdog=0
+```
+
+Small threads on the need of these permissions:
+https://unix.stackexchange.com/questions/14227/
+https://stackoverflow.com/questions/51911368/
+
+Security implications:
+https://www.kernel.org/doc/html/latest/admin-guide/perf-security.html
+https://unix.stackexchange.com/questions/519070/
+
+
+### Building ROOT for profiling
+
+```
+cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_CXX_FLAGS="-fno-omit-frame-pointer" -Dccache=ON ..
+```
+
+Build options: https://root.cern/install/build_from_source/#all-build-options
+
+Remark: you can disable options, which are enabled by default, by setting them to OFF.
+
+
+## Compiling the example and getting the input file
+
+### Compiling the example
+
+```
+g++ -O2 -g -fno-omit-frame-pointer -Wall -Wextra -Wpedantic -o rdf rdf.cpp $(root-config --cflags --libs)
+```
+
+### Getting the input file
+
+This input file is only ~ 100 MB.
+
+```
+mkdir input
+wget Run2012B_SingleMu_1M https://root.cern/files/rootbench/Run2012B_SingleMu_1M.root -P input
+```
+
+Note that since the file is so small, if you work with only 1 instance of the file, you will notice that most of the work is spent in initialization.
+
+However, you can initialize the dataframe with multiple instances of the file, and then you will see that the work is spent in the event loop. So you do not need to make neither hard or soft links to the file. Fyi: there is a some small difference in I/O performance between these options, studied at https://indico.cern.ch/event/1191473/.
+
+## Profiling the example
+
+### Perf stat
+
+```
+perf stat ./rdf original input/ 30
+```
+
+The result should be similar to:
+```
+Info in <TCanvas::Print>: png file original_rdf.png has been created
+mean h1 is 26.435
+mean h2 is 0.553772
+
+ Performance counter stats for './rdf original input/ 30':
+
+         53 065,07 msec task-clock                #    1,000 CPUs utilized          
+               233      context-switches          #    4,391 /sec                   
+                 0      cpu-migrations            #    0,000 /sec                   
+           181 519      page-faults               #    3,421 K/sec                  
+   190 973 918 953      cycles                    #    3,599 GHz                    
+   376 844 363 732      instructions              #    1,97  insn per cycle         
+    58 532 205 284      branches                  #    1,103 G/sec                  
+     1 204 603 018      branch-misses             #    2,06% of all branches        
+
+      53,068551395 seconds time elapsed
+
+      52,082176000 seconds user
+       0,914011000 seconds sys
+```
+
+In addition, on Intel CPUs, you can get more insight already here with either:
+```
+perf stat --topdown -a ./rdf original input/ 30
+```
+or equivalently:
+```
+perf stat -M TopdownL1 ./rdf original input/ 30
+```
+or also:
+```
+perf stat -M Frontend_Bound,Backend_Bound,Bad_Speculation,Retiring time
+```
+
+All of the above should give:
+```
+mean h1 is 26.435
+mean h2 is 0.553772
+
+ Performance counter stats for './rdf original input/ 30':
+
+   437 075 361 555      uops_issued.any           #     0,12 Bad_Speculation
+                                                  #     0,22 Backend_Bound            (33,33%)                              
+   191 218 334 772      cycles
+                                                  # 764873339088,00 SLOTS
+                                                  #     0,49 Retiring
+                                                  #     0,17 Frontend_Bound           (33,33%)                              
+   128 748 925 064      idq_uops_not_delivered.core                                     (33,33%)                            
+     7 158 428 964      int_misc.recovery_cycles                                      (33,33%)                              
+   373 401 307 787      uops_retired.retire_slots                                     (33,33%)                              
+     1 311 634 367      cpu_clk_unhalted.one_thread_active #     1,97 CoreIPC_SMT              (33,34%)                     
+   376 724 917 454      inst_retired.any          # 376724917454,00 Instructions      (33,34%)                              
+     1 312 820 545      cpu_clk_unhalted.ref_xclk                                     (33,34%)                              
+   191 017 221 261      cpu_clk_unhalted.thread                                       (33,34%)                              
+   376 836 235 015      inst_retired.any          #     1,97 CoreIPC                  (66,67%)                              
+   191 049 315 819      cycles                                                        (66,67%)                              
+
+      53,202590594 seconds time elapsed
+
+      52,025563000 seconds user
+       0,952009000 seconds sys
+
+```
+
+An AMD CPU would not accept the first two options, but similarly to the last option, you can do:
+```
+perf stat -M Backend_Bound,Bad_Speculation,Retiring time
+```
+(As of the moment of this upload (Feb '23) - there is no Frontend_Bound metric for AMD CPUs!)
+
+### Perf record and producing a flamegraph
+
+#### Perf record
+
+```
+perf record -o original.data -F 99 --call-graph=dwarf ./rdf original input/ 30
+```
+
+One very useful approach is to see which event was reported as a bottleneck by perf stat, and then to record the events that are related to that bottleneck. You can specify this event with the `-e` option of perf record.
+
+#### Perf report
+
+Read the produced data file with perf report:
+
+```
+perf report -i original.data
+```
+
+So far, you should be able to see where is the bottleneck, but not why, e.g. the place with the most samples. To understand what is the cause, you would need to annotate functions with "a" inside of the report. Refer to the help ("h") of perf report for more information.
+
+#### Flamegraph
+
+It is often highly useful to visually argue about performance. For this, you can use flamegraphs. And you cannot properly open a data file from different machine! So to convince others about your results, you need to produce a flamegraph.
+
+You need to get some necessary tools first:
+
+```
+wget https://raw.githubusercontent.com/brendangregg/FlameGraph/master/stackcollapse-perf.pl
+wget https://raw.githubusercontent.com/brendangregg/FlameGraph/master/flamegraph.pl
+chmod +x stackcollapse-perf.pl flamegraph.pl
+```
+
+
+You can produce a flamegraph with the following command:
+
+```
+mkdir graphs
+perf script -i original.data | ./stackcollapse-perf.pl | ./flamegraph.pl -w 1500 --colors java > graphs/original.svg
+```
+
+This should already give you a good idea of what is going on.
+
+For the purpose of this example, convince yourself why find_trijet is expensive. Then pick the optimized version, which you can invoke by simply changing the first extra argument of the executable to "direct".
+
+First of all, assure that the results are identical.
+
+Then you can go through the same steps as above, but with the optimized version.
+
+
+#### Differential flamegraph
+
+As a final stroke, you would like to assert that your optimization solved the problem that you were trying to solve, and it did not introduce any new overheads.
+
+You can compare multiple perf reports with perf diff, e.g.:
+
+```
+perf diff original.data transposed.data direct.data
+```
+
+And you can visualize the difference between two reports with a differential flamegraph.
+
+You would first need an extra tool:
+```
+wget https://raw.githubusercontent.com/brendangregg/FlameGraph/master/difffolded.pl
+chmod +x difffolded.pl
+```
+
+Then you can produce the differential flamegraph with the following commands:
+
+```
+perf script -i original.data | ./stackcollapse-perf.pl > out.folded1
+perf script -i direct.data | ./stackcollapse-perf.pl > out.folded2
+./difffolded.pl out.folded1 out.folded2 | ./flamegraph.pl -w 1500 > graphs/diff.svg
+```
+
+## Sources:
+
+* [Perf man page](https://man7.org/linux/man-pages/man1/perf.1.html)
+* [Example Perf commands](https://www.brendangregg.com/perf.html)
---
+++ rdf.cpp
@@ -0,0 +1,292 @@
+#include "Math/Vector4D.h"
+#include "ROOT/RDataFrame.hxx"
+#include "ROOT/RVec.hxx"
+#include "TCanvas.h"
+
+#include "ROOT/TTreeProcessorMT.hxx"
+
+#include <algorithm>
+#include <iostream>
+
+template <typename T> using Vec = const ROOT::RVec<T> &;
+using ROOT::Math::XYZTVector;
+
+// rewriting the combinations function
+ROOT::RVec<ROOT::RVec<std::size_t>> combinations(std::size_t s, std::size_t n) {
+  ROOT::RVec<std::size_t> indices(s);
+  std::iota(indices.begin(), indices.end(), 0);
+
+  const auto innersize = [=] {
+    std::size_t inners = s - n + 1;
+    for (std::size_t m = s - n + 2; m <= s; ++m)
+      inners *= m;
+
+    std::size_t factn = 1;
+    for (std::size_t i = 2; i <= n; ++i)
+      factn *= i;
+    inners /= factn;
+
+    return inners;
+  }();
+
+  ROOT::RVec<ROOT::RVec<std::size_t>> c(n, ROOT::RVec<std::size_t>(innersize));
+
+  std::size_t inneridx = 0;
+  for (std::size_t k = 0; k < n; k++)
+    c[k][inneridx] = indices[k];
+  ++inneridx;
+
+  while (true) {
+    bool run_through = true;
+    long i = n - 1;
+    for (; i >= 0; i--) {
+      if (indices[i] != i + s - n) {
+        run_through = false;
+        break;
+      }
+    }
+    if (run_through) {
+      return c;
+    }
+    indices[i]++;
+    for (long j = i + 1; j < (long)n; j++)
+      indices[j] = indices[j - 1] + 1;
+    for (std::size_t k = 0; k < n; k++)
+      c[k][inneridx] = indices[k];
+    ++inneridx;
+  }
+}
+
+ROOT::RVec<std::size_t> original_find_trijet(Vec<XYZTVector> jets) {
+  const auto c = ROOT::VecOps::Combinations(jets, 3);
+
+  float distance = 1e9;
+  const auto top_mass = 172.5;
+  std::size_t idx = 0;
+  for (auto i = 0u; i < c[0].size(); i++) {
+    auto p1 = jets[c[0][i]];
+    auto p2 = jets[c[1][i]];
+    auto p3 = jets[c[2][i]];
+    const auto tmp_mass = (p1 + p2 + p3).mass();
+    const auto tmp_distance = std::abs(tmp_mass - top_mass);
+    if (tmp_distance < distance) {
+      distance = tmp_distance;
+      idx = i;
+    }
+  }
+
+  return {c[0][idx], c[1][idx], c[2][idx]};
+}
+
+ROOT::RVec<std::size_t> equivalent_find_trijet(Vec<XYZTVector> jets) {
+
+  const auto c = combinations(jets.size(), 3);
+
+  float distance = 1e9;
+  const auto top_mass = 172.5;
+  std::size_t idx = 0;
+  for (auto i = 0u; i < c[0].size(); i++) {
+    auto p1 = jets[c[0][i]];
+    auto p2 = jets[c[1][i]];
+    auto p3 = jets[c[2][i]];
+    const auto tmp_mass = (p1 + p2 + p3).mass();
+    const auto tmp_distance = std::abs(tmp_mass - top_mass);
+    if (tmp_distance < distance) {
+      distance = tmp_distance;
+      idx = i;
+    }
+  }
+
+  return {c[0][idx], c[1][idx], c[2][idx]};
+}
+
+// worse memory access pattern! think why!
+ROOT::RVec<std::size_t> transposed_find_trijet(Vec<XYZTVector> jets) {
+
+  const auto c = combinations(3, jets.size());
+
+  float distance = 1e9;
+  const auto top_mass = 172.5;
+  std::size_t idx = 0;
+  for (auto i = 0u; i < c.size(); i++) {
+    auto p1 = jets[c[i][0]];
+    auto p2 = jets[c[i][1]];
+    auto p3 = jets[c[i][2]];
+    const auto tmp_mass = (p1 + p2 + p3).mass();
+    const auto tmp_distance = std::abs(tmp_mass - top_mass);
+    if (tmp_distance < distance) {
+      distance = tmp_distance;
+      idx = i;
+    }
+  }
+
+  return {c[idx][0], c[idx][1], c[idx][2]};
+}
+
+// make use of the fact that we know the size of the combinations
+// no need to create extra 2d vector
+ROOT::RVec<std::size_t> direct_find_trijet(Vec<XYZTVector> jets) {
+  constexpr std::size_t n = 3;
+  float distance = 1e9;
+  const auto top_mass = 172.5;
+  std::size_t idx1 = 0, idx2 = 1, idx3 = 2;
+
+  for (std::size_t i = 0; i <= jets.size() - n; i++) {
+    auto p1 = jets[i];
+    for (std::size_t j = i + 1; j <= jets.size() - n + 1; j++) {
+      auto p2 = jets[j];
+      for (std::size_t k = j + 1; k <= jets.size() - n + 2; k++) {
+        auto p3 = jets[k];
+        const auto tmp_mass = (p1 + p2 + p3).mass();
+        const auto tmp_distance = std::abs(tmp_mass - top_mass);
+        if (tmp_distance < distance) {
+          distance = tmp_distance;
+          idx1 = i;
+          idx2 = j;
+          idx3 = k;
+        }
+      }
+    }
+  }
+
+  return {idx1, idx2, idx3};
+}
+
+// TODO: now p1.mass() + p2.mass() != (p1 + p2).mass
+// TODO: find a proxy function which is distributive
+// Disclaimer: Might be impossible!
+// a two-pointer approach, O(n^2) time and O(n) space
+// need to understand if we are allowed to reorder to original vector
+// if so, can solve the problem in O(1) space
+// https://leetcode.com/problems/3sum-closest/
+ROOT::RVec<std::size_t> nsquare_find_trijet(Vec<XYZTVector> jets) {
+  float distance = 1e9;
+  const auto top_mass = 172.5;
+  std::size_t idx1 = 0, idx2 = 1, idx3 = 2;
+
+  ROOT::RVec<std::size_t> inds(jets.size());
+  std::iota(inds.begin(), inds.end(), 0);
+  std::sort(inds.begin(), inds.end(), [&jets](const auto &a, const auto &b) {
+    return jets[a].mass() < jets[b].mass();
+  });
+
+  for (std::size_t i = 0; i <= jets.size() - 2; ++i) {
+    std::size_t j = i + 1, k = jets.size() - 1;
+    while (j < k) {
+      const auto tmp_mass =
+          (jets[inds[i]] + jets[inds[j]] + jets[inds[k]]).mass();
+      if (tmp_mass == top_mass)
+        return {inds[i], inds[j], inds[k]};
+      const auto tmp_distance = std::abs(tmp_mass - top_mass);
+      if (tmp_distance < distance) {
+        distance = tmp_distance;
+        idx1 = inds[i];
+        idx2 = inds[j];
+        idx3 = inds[k];
+      }
+      if (tmp_mass < top_mass)
+        ++j;
+      else
+        --k;
+    }
+  }
+  return {idx1, idx2, idx3};
+}
+
+float trijet_pt(Vec<float> pt, Vec<float> eta, Vec<float> phi, Vec<float> mass,
+                Vec<std::size_t> idx) {
+  auto p1 = ROOT::Math::PtEtaPhiMVector(pt[idx[0]], eta[idx[0]], phi[idx[0]],
+                                        mass[idx[0]]);
+  auto p2 = ROOT::Math::PtEtaPhiMVector(pt[idx[1]], eta[idx[1]], phi[idx[1]],
+                                        mass[idx[1]]);
+  auto p3 = ROOT::Math::PtEtaPhiMVector(pt[idx[2]], eta[idx[2]], phi[idx[2]],
+                                        mass[idx[2]]);
+  return (p1 + p2 + p3).pt();
+}
+
+void rdataframe(const std::string &method_find_trijet,
+                const std::string &file_path, int nfiles) {
+  std::function<ROOT::RVec<std::size_t>(const ROOT::RVec<XYZTVector> &)>
+      find_trijet;
+
+  if (method_find_trijet == "original")
+    find_trijet = original_find_trijet;
+  else if (method_find_trijet == "equivalent")
+    find_trijet = equivalent_find_trijet;
+  else if (method_find_trijet == "transposed")
+    find_trijet = transposed_find_trijet;
+  else if (method_find_trijet == "direct")
+    find_trijet = direct_find_trijet;
+  else if (method_find_trijet == "nsquare")
+    find_trijet = nsquare_find_trijet;
+
+  using ROOT::Math::PtEtaPhiMVector;
+  using ROOT::VecOps::Construct;
+
+  // it would be very interesting to look at the MT case
+  // (but this would not fit in the 1 hour time limit, sorry)
+  // ROOT::EnableImplicitMT();
+
+  ROOT::RDataFrame df("Events",
+                      std::vector<std::string>(
+                          nfiles, file_path + "Run2012B_SingleMu_1M.root"));
+  auto df2 = df.Filter([](unsigned int n) { return n >= 3; }, {"nJet"},
+                       "At least three jets")
+                 .Define("JetXYZT",
+                         [](Vec<float> pt, Vec<float> eta, Vec<float> phi,
+                            Vec<float> m) {
+                           return Construct<XYZTVector>(
+                               Construct<PtEtaPhiMVector>(pt, eta, phi, m));
+                         },
+                         {"Jet_pt", "Jet_eta", "Jet_phi", "Jet_mass"})
+                 .Define("Trijet_idx", find_trijet, {"JetXYZT"});
+  auto h1 =
+      df2.Define("Trijet_pt", trijet_pt,
+                 {"Jet_pt", "Jet_eta", "Jet_phi", "Jet_mass", "Trijet_idx"})
+          .Histo1D<float>({"", ";Trijet pt (GeV);N_{Events}", 100, 15, 40},
+                          "Trijet_pt");
+  auto h2 =
+      df2.Define("Trijet_leadingBtag",
+                 [](const ROOT::RVec<float> &btag,
+                    const ROOT::RVec<std::size_t> &idx) {
+                   return Max(Take(btag, idx));
+                 },
+                 {"Jet_btag", "Trijet_idx"})
+          .Histo1D<float>({"", ";Trijet leading b-tag;N_{Events}", 100, 0, 1},
+                          "Trijet_leadingBtag");
+
+  TCanvas c;
+  c.Divide(2, 1);
+  c.cd(1);
+  h1->Draw();
+  c.cd(2);
+  h2->Draw();
+  // check the canvas as a sanity check
+  c.SaveAs((method_find_trijet + "_rdf.png").data());
+
+  std::cout << "mean h1 is " << h1->GetMean() << std::endl;
+  std::cout << "mean h2 is " << h2->GetMean() << std::endl;
+}
+
+int main(int argc, const char *argv[]) {
+  if (argc != 4) {
+    std::cout << "Usage: ./rdf <method> <filepath> <nfiles>" << std::endl;
+    return 1;
+  }
+  std::string method_find_trijet = argv[1];
+  std::string filepath = argv[2];
+  int nf = atoi(argv[3]);
+  std::vector<std::string> valid_methods = {"original", "equivalent",
+                                            "transposed", "direct", "nsquare"};
+  if (std::find(valid_methods.begin(), valid_methods.end(),
+                method_find_trijet) == valid_methods.end()) {
+    std::cout << "Invalid method: " << method_find_trijet << std::endl;
+    std::cout << "Valid methods are: ";
+    for (auto &m : valid_methods) {
+      std::cout << m << " ";
+    }
+    std::cout << "Defaulting to original" << std::endl;
+    method_find_trijet = "original";
+  }
+  rdataframe(method_find_trijet, filepath, nf);
+}
